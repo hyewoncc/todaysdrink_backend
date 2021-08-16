28979f225c2e5e3014230067a88104c9b6e41944
hyewoncc <hyewoncc@gmail.com>
2021-08-16 21:38:04 +0900

    create project

---
+++ src/main/java/com/todaysdrink/todaysdrink/TodaysdrinkApplication.java
@@ -0,0 +1,13 @@
+package com.todaysdrink.todaysdrink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysdrinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaysdrinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todaysdrink/todaysdrink/TodaysdrinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todaysdrink.todaysdrink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaysdrinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
