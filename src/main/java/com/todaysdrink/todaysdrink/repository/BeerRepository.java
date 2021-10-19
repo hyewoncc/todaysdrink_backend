22981e1c0e71c8ea84433d900ca8db881e35f00d
hyewoncc <hyewoncc@gmail.com>
2021-10-19 23:47:45 +0900

    Add RecommendService class

--- src/main/java/com/todaysdrink/todaysdrink/repository/BeerRepository.java
+++ src/main/java/com/todaysdrink/todaysdrink/repository/BeerRepository.java
@@ -7,7 +7,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BeerRepository extends JpaRepository<Beer, Long> {
     Page<Beer> findByCountry(Country country, Pageable pageable);
     Page<Beer> findByBeerType(BeerType beerType, Pageable pageable);
+    List<Beer> findTop5ByBeerTypeOrderByLike(BeerType beerType);
 }
---
+++ src/main/java/com/todaysdrink/todaysdrink/service/RecommendService.java
@@ -0,0 +1,23 @@
+package com.todaysdrink.todaysdrink.service;
+
+import com.todaysdrink.todaysdrink.domain.Beer;
+import com.todaysdrink.todaysdrink.repository.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class RecommendService {
+
+    private final BeerRepository beerRepository;
+
+    // 동일 타입 기준 좋아요 순 5개 맥주 추천
+    public List<Beer> getRecommendByType(Beer beer) {
+        List<Beer> result = beerRepository.findTop5ByBeerTypeOrderByLike(beer.getBeerType());
+        return result;
+    }
+}
