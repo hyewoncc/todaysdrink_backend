79bf5964a166dcbe901f8e932e04fe85e8590c52
hyewoncc <hyewoncc@gmail.com>
2021-10-26 23:35:36 +0900

    Add recommend controller

--- src/main/java/com/todaysdrink/todaysdrink/api/controller/RecommendController.java
+++ src/main/java/com/todaysdrink/todaysdrink/api/controller/RecommendController.java
@@ -1,2 +1,40 @@
-package com.todaysdrink.todaysdrink.api.controller;public class RecommendController {
+package com.todaysdrink.todaysdrink.api.controller;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.todaysdrink.todaysdrink.domain.Beer;
+import com.todaysdrink.todaysdrink.dto.BeerDto;
+import com.todaysdrink.todaysdrink.service.BeerService;
+import com.todaysdrink.todaysdrink.service.RecommendService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/recommend")
+public class RecommendController {
+    
+    private final BeerService beerService;
+    private final RecommendService recommendService;
+    
+    @GetMapping("/beer/{id}")
+    public ResponseEntity<CollectionModel<EntityModel<BeerDto>>> recommendByType(@PathVariable Long id) {
+        Optional<Beer> beer = beerService.getOneBeer(id);
+        List<Beer> recommendBeers = recommendService.getRecommendByType(beer.get());
+        List<EntityModel<BeerDto>> beerDtos = recommendBeers.stream()
+                .map(b -> EntityModel.of(new BeerDto(b),
+                        linkTo(methodOn(BeerController.class).findOne(b.getId())).withSelfRel()))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(CollectionModel.of(beerDtos));
+    }
 }
