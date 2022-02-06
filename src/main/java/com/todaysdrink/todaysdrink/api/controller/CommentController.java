0e014f8700d4fbe8bc97a132b3ed90fe0aa5df19
hyewoncc <hyewoncc@gmail.com>
2022-02-06 22:55:43 +0900

    Fix delete unused imports

--- src/main/java/com/todaysdrink/todaysdrink/api/controller/CommentController.java
+++ src/main/java/com/todaysdrink/todaysdrink/api/controller/CommentController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
